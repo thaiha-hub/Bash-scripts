852793a63df8b83e848e4119c036687f9d9d9715
thaiha-hub <devops.engineer0528@gmail.com>
2024-11-08 22:49:10 +0200

    Update bash1.sh

--- Excercises/bash1.sh
+++ Excercises/bash1.sh
@@ -4,3 +4,7 @@ name=$(whoami)
 location=$(pwd)
 
 echo "Hej $name, du befinner dig i katalogen $location."
+
+
+#no spaces when writing variables
+#The script needs the first line in order to be recognized
